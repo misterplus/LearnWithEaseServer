244ad673d60a84ed06c4e2fe4ee5bc9bf80f0948
misterplus <whiteplayz45@gmail.com>
2021-05-05 19:10:46 +0800

    Update user definition

--- src/main/java/team/one/lwes/bean/UserInfo.java
+++ src/main/java/team/one/lwes/bean/UserInfo.java
@@ -3,13 +3,11 @@ package team.one.lwes.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import team.one.lwes.enums.Background;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfo {
-    private int age, grade;
-    private Background bak;
+    private int age, grade, bak;
     private String province, city, area, school;
 }
