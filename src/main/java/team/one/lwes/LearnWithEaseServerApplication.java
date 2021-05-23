c15c2ecbb9d0ac21f905510c8369b731e01c260f
misterplus <whiteplayz45@gmail.com>
2021-05-23 20:57:45 +0800

    test

--- src/main/java/team/one/lwes/LearnWithEaseServerApplication.java
+++ src/main/java/team/one/lwes/LearnWithEaseServerApplication.java
@@ -4,17 +4,10 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextClosedEvent;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class LearnWithEaseServerApplication {
@@ -55,29 +48,5 @@ public class LearnWithEaseServerApplication {
         return connector;
     }
 
-    private class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
-        private final int waitTime = 10;
-        private volatile Connector connector;
 
-        @Override
-        public void customize(Connector connector) {
-            this.connector = connector;
-        }
-
-        @Override
-        public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
-            this.connector.pause();
-            Executor executor = this.connector.getProtocolHandler().getExecutor();
-            try {
-                if (executor instanceof ThreadPoolExecutor) {
-                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                    threadPoolExecutor.shutdown();
-                    threadPoolExecutor.awaitTermination(waitTime, TimeUnit.SECONDS);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
 }
