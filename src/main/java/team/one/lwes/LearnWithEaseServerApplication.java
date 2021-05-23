c1bc6eb6f81e26a4fbac5afaacf3edee6980edd7
misterplus <whiteplayz45@gmail.com>
2021-05-23 14:22:00 +0800

    jar

--- src/main/java/team/one/lwes/LearnWithEaseServerApplication.java
+++ src/main/java/team/one/lwes/LearnWithEaseServerApplication.java
@@ -2,16 +2,9 @@ package team.one.lwes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearnWithEaseServerApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(LearnWithEaseServerApplication.class);
-    }
+public class LearnWithEaseServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnWithEaseServerApplication.class, args);
