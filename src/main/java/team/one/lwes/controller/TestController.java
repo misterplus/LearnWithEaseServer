e096d7c96b5d6e1b3e1a89e9dd7fe3bb562a1c92
misterplus <whiteplayz45@gmail.com>
2021-05-24 14:56:11 +0800

    test

---
+++ src/main/java/team/one/lwes/controller/TestController.java
@@ -0,0 +1,14 @@
+package team.one.lwes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.one.lwes.Constants;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/secret")
+    public String getSecret() {
+        return Constants.getInstance().APP_SECRET;
+    }
+}
